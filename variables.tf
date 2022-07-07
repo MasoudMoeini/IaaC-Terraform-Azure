2f8817c461680253af8306b26e321fe7f5529757
MasoudMoeini <Masoud.Moeini@Studium.uni-hamburg.de>
2022-07-07 09:35:40 +0200

    update

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
-variable "resource_group_name" {
-  default = "myTFResourceGroup"
+variable "prefix" {
+  description = "The Prefix used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which the resources in this example should exist"
 }
